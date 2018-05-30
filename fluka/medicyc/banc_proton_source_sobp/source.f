a1d17628f7cfdccd439540cadb7788ecf7247de0
Petter Hofverberg <petter.hofverberg@gmail.com>
2018-05-30 15:52:09 +0200

    Add medicyc fluka simulations from adela

---
+++ fluka/medicyc/banc_proton_source_sobp/source.f
@@ -0,0 +1,242 @@
+*$ CREATE SOURCE.FOR
+*COPY SOURCE
+*
+*=== source ===========================================================*
+*
+      SUBROUTINE SOURCE ( NOMORE )
+
+      INCLUDE '(DBLPRC)'
+      INCLUDE '(DIMPAR)'
+      INCLUDE '(IOUNIT)'
+*
+*----------------------------------------------------------------------*
+*                                                                      *
+*     Copyright (C) 1990-2010      by    Alfredo Ferrari & Paola Sala  *
+*     All Rights Reserved.                                             *
+*                                                                      *
+*                                                                      *
+*     New source for FLUKA9x-FLUKA20xy:                                *
+*                                                                      *
+*     Created on 07 January 1990   by    Alfredo Ferrari & Paola Sala  *
+*                                                   Infn - Milan       *
+*                                                                      *
+*     Last change on  17-Oct-10    by    Alfredo Ferrari               *
+*                                                                      *
+*  This is just an example of a possible user written source routine.  *
+*  note that the beam card still has some meaning - in the scoring the *
+*  maximum momentum used in deciding the binning is taken from the     *
+*  beam momentum.  Other beam card parameters are obsolete.            *
+*                                                                      *
+*       Output variables:                                              *
+*                                                                      *
+*              Nomore = if > 0 the run will be terminated              *
+*                                                                      *
+*----------------------------------------------------------------------*
+*
+      INCLUDE '(BEAMCM)'
+      INCLUDE '(FHEAVY)'
+      INCLUDE '(FLKSTK)'
+      INCLUDE '(IOIOCM)'
+      INCLUDE '(LTCLCM)'
+      INCLUDE '(PAPROP)'
+      INCLUDE '(SOURCM)'
+      INCLUDE '(SUMCOU)'
+*
+      LOGICAL LFIRST
+*
+      SAVE LFIRST
+      DATA LFIRST / .TRUE. /
+
+      PARAMETER (NMAX = 326)  
+*BAK: NMAX is total number of lines in "source.dat" file
+      DIMENSION HLE(NMAX), HLP(NMAX), HLPN(NMAX)
+*BAK: HLE(NMAX) to keep the energies, HLP(NMAX) to keep the probabilities
+*BAK: HLPN(NMAX) to keep the normalized probabilities, 100 % = 1
+      REAL*8 FE, FP, SUMP
+*BAK: FE and FP will be used to read Energy and Probability each
+*     when fluka reads "source.dat" file.
+*     SUMP will be used to sum the probabilities during reading the file
+      INTEGER FINPUT, PE 
+*BAK: This PE integer will be used when we are searching an energy.
+
+*======================================================================*
+*                                                                      *
+*                 BASIC VERSION                                        *
+*                                                                      *
+*======================================================================*
+      NOMORE = 0
+*  +-------------------------------------------------------------------*
+*  |  First call initializations:
+      IF ( LFIRST ) THEN
+*  |  *** The following 3 cards are mandatory ***
+         TKESUM = ZERZER
+         LFIRST = .FALSE.
+         LUSSRC = .TRUE.
+*  |  *** User initialization ***
+         FINPUT = NINT(WHASOU(1)) !Read WHAT(1) from SOURCE card on inp file
+         SUMP = 0.  !Initialize
+         DO I = 1, NMAX
+            READ(FINPUT,190,END=770) FE, FP
+*            PRINT *, FE, FP
+            HLE(I) = FE/1E3 ! Energy unit MeV->GeV
+            HLP(I) = FP
+            SUMP = SUMP + FP
+         END DO
+ 760     CONTINUE
+         PRINT *, 'error'
+ 770     CONTINUE
+ 190     FORMAT (E10.4,E14.7)
+*BAK:    Read the file from 1 to NMAX line, keep on LE, LP.
+*BAK:    SUMP is summing all the probabilities
+         DO I = 1, NMAX
+           IF (I.EQ.1) THEN
+             HLPN(I) = HLP(I)/SUMP
+           ELSE
+*             IF ((HLP(I).LE.1E-12)) THEN
+*               HLPN(I) = 0.
+*             ELSE
+               HLPN(I) = HLPN(I-1)+HLP(I)/SUMP
+*             END IF
+           END IF
+         END DO
+*BAK:    Keep the normailzed probabilities.
+
+      END IF
+*  |
+*  +-------------------------------------------------------------------*
+*  Push one source particle to the stack. Note that you could as well
+*  push many but this way we reserve a maximum amount of space in the
+*  stack for the secondaries to be generated
+*  Npflka is the stack counter: of course any time source is called it
+*  must be =0
+      NPFLKA = NPFLKA + 1
+*  Wt is the weight of the particle
+      WTFLK  (NPFLKA) = ONEONE
+      WEIPRI = WEIPRI + WTFLK (NPFLKA)
+*  Particle type (1=proton.....). Ijbeam is the type set by the BEAM
+*  card
+*  +-------------------------------------------------------------------*
+*  |  (Radioactive) isotope:
+      IF ( IJBEAM .EQ. -2 .AND. LRDBEA ) THEN
+         IARES  = IPROA
+         IZRES  = IPROZ
+         IISRES = IPROM
+         CALL STISBM ( IARES, IZRES, IISRES )
+         IJHION = IPROZ  * 1000 + IPROA
+         IJHION = IJHION * 100 + KXHEAV
+         IONID  = IJHION
+         CALL DCDION ( IONID )
+         CALL SETION ( IONID )
+*  |
+*  +-------------------------------------------------------------------*
+*  |  Heavy ion:
+      ELSE IF ( IJBEAM .EQ. -2 ) THEN
+         IJHION = IPROZ  * 1000 + IPROA
+         IJHION = IJHION * 100 + KXHEAV
+         IONID  = IJHION
+         CALL DCDION ( IONID )
+         CALL SETION ( IONID )
+         ILOFLK (NPFLKA) = IJHION
+*  |  Flag this is prompt radiation
+         LRADDC (NPFLKA) = .FALSE.
+*  |  Group number for "low" energy neutrons, set to 0 anyway
+         IGROUP (NPFLKA) = 0
+*  |
+*  +-------------------------------------------------------------------*
+*  |  Normal hadron:
+      ELSE
+         IONID = IJBEAM
+         ILOFLK (NPFLKA) = IJBEAM
+*  |  Flag this is prompt radiation
+         LRADDC (NPFLKA) = .FALSE.
+*  |  Group number for "low" energy neutrons, set to 0 anyway
+         IGROUP (NPFLKA) = 0
+      END IF
+*  |
+*  +-------------------------------------------------------------------*
+*  From this point .....
+*  Particle generation (1 for primaries)
+      LOFLK  (NPFLKA) = 1
+*  User dependent flag:
+      LOUSE  (NPFLKA) = 0
+*  No channeling:
+      LCHFLK (NPFLKA) = .FALSE.
+      DCHFLK (NPFLKA) = ZERZER
+*  User dependent spare variables:
+      DO 100 ISPR = 1, MKBMX1
+         SPAREK (ISPR,NPFLKA) = ZERZER
+ 100  CONTINUE
+*  User dependent spare flags:
+      DO 200 ISPR = 1, MKBMX2
+         ISPARK (ISPR,NPFLKA) = 0
+ 200  CONTINUE
+*  Save the track number of the stack particle:
+      ISPARK (MKBMX2,NPFLKA) = NPFLKA
+      NPARMA = NPARMA + 1
+      NUMPAR (NPFLKA) = NPARMA
+      NEVENT (NPFLKA) = 0
+      DFNEAR (NPFLKA) = +ZERZER
+*  ... to this point: dont change anything
+*  Particle age (s)
+      AGESTK (NPFLKA) = +ZERZER
+      AKNSHR (NPFLKA) = -TWOTWO
+*  Kinetic energy of the particle (GeV)
+      !TKEFLK (NPFLKA) = SQRT ( PBEAM**2 + AM (IONID)**2 ) - AM (IONID)
+*BAK: Sampling the energy below
+      RN = FLRNDM() ! Get random number between 0 and 1
+      DO I = 1, NMAX
+         PE = I
+         IF (RN.LE.HLPN(I) ) THEN
+           GOTO 210
+         END IF    
+      END DO   
+ 210  CONTINUE
+      TKEFLK (NPFLKA) = HLE(PE) ! BAK: Set Energy
+
+*BAK: We set the kinetic energy above, so we should calculate the momentum below
+
+*  Particle momentum
+      !PMOFLK (NPFLKA) = PBEAM ! BAK: Use the below lines instead of this line
+      PMOFLK (NPFLKA) = SQRT ( TKEFLK (NPFLKA) * ( TKEFLK (NPFLKA)
+     &                       + TWOTWO * AM (IONID) ) )
+*  Cosines (tx,ty,tz)
+      TXFLK  (NPFLKA) = UBEAM
+      TYFLK  (NPFLKA) = VBEAM
+      TZFLK  (NPFLKA) = WBEAM
+*     TZFLK  (NPFLKA) = SQRT ( ONEONE - TXFLK (NPFLKA)**2
+*    &                       - TYFLK (NPFLKA)**2 )
+*  Polarization cosines:
+      TXPOL  (NPFLKA) = -TWOTWO
+      TYPOL  (NPFLKA) = +ZERZER
+      TZPOL  (NPFLKA) = +ZERZER
+*  Particle coordinates
+      XFLK   (NPFLKA) = XBEAM
+      YFLK   (NPFLKA) = YBEAM
+      ZFLK   (NPFLKA) = ZBEAM
+*  Calculate the total kinetic energy of the primaries: dont change
+      IF ( ILOFLK (NPFLKA) .EQ. -2 .OR. ILOFLK (NPFLKA) .GT. 100000 )
+     &   THEN
+         TKESUM = TKESUM + TKEFLK (NPFLKA) * WTFLK (NPFLKA)
+      ELSE IF ( ILOFLK (NPFLKA) .NE. 0 ) THEN
+         TKESUM = TKESUM + ( TKEFLK (NPFLKA) + AMDISC (ILOFLK(NPFLKA)) )
+     &          * WTFLK (NPFLKA)
+      ELSE
+         TKESUM = TKESUM + TKEFLK (NPFLKA) * WTFLK (NPFLKA)
+      END IF
+      RADDLY (NPFLKA) = ZERZER
+*  Here we ask for the region number of the hitting point.
+*     NREG (NPFLKA) = ...
+*  The following line makes the starting region search much more
+*  robust if particles are starting very close to a boundary:
+      CALL GEOCRS ( TXFLK (NPFLKA), TYFLK (NPFLKA), TZFLK (NPFLKA) )
+      CALL GEOREG ( XFLK  (NPFLKA), YFLK  (NPFLKA), ZFLK  (NPFLKA),
+     &              NRGFLK(NPFLKA), IDISC )
+*  Do not change these cards:
+      CALL GEOHSM ( NHSPNT (NPFLKA), 1, -11, MLATTC )
+      NLATTC (NPFLKA) = MLATTC
+      CMPATH (NPFLKA) = ZERZER
+      CALL SOEVSV
+      RETURN
+*=== End of subroutine Source =========================================*
+      END
+
